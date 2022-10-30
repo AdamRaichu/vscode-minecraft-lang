7c0ad4bb1cf0f665e74571429436afe87cd59b1b
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-30 15:07:40 -0400

    remove a bunch of comments

--- extension.js
+++ extension.js
@@ -2,8 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate() {
   vscode.languages.registerHoverProvider("mc-lang", {
     provideHover(document, position, token) {
