848872ad25e0f0929bb149533dd812ea9fa02238
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-30 15:26:40 -0400

    remove export statement bc it was generating errors
    remove main from package

--- extension.js
+++ extension.js
@@ -2,13 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 
-export function activate() {
-  vscode.languages.registerHoverProvider("mc-lang", {
-    provideHover(document, position, token) {
-      return new Hover("I am a hover!");
-    },
-  });
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
+vscode.languages.registerHoverProvider("mc-lang", {
+  provideHover(document, position, token) {
+    return new Hover("I am a hover!");
+  },
+});
