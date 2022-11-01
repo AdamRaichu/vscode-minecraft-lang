7b4ad79d6f0ba0b0621852dad35c6901a1c8fd5e
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-11-01 09:04:07 -0400

    push more changes

--- extension.js
+++ extension.js
@@ -4,14 +4,26 @@ var index1 = {
   "": "",
   accessibility: "For keys pertaining to text-to-speech translation",
 };
+var index2 = {};
 
 vscode.languages.registerHoverProvider("mc-lang", {
   provideHover(document, position, token) {
     var word = document.getText(document.getWordRangeAtPosition(position));
-    if (word in index1) {
-      return new vscode.Hover(`**${word}**\n\n${index1[word]}`);
+    var line = document.lineAt(position);
+    if (line.text.startsWith(word)) {
+      if (word in index1) {
+        // Top level keys
+        return new vscode.Hover(`**${word}**\n\n${index1[word]}`);
+      } else {
+        return new vscode.Hover("No information available");
+      }
     } else {
-      return new vscode.Hover("No information available");
+      if (word in index2) {
+        // Anything else
+        return new vscode.Hover(
+          `**${word}**\n\n${index2[word]}\n\n*Hover hints do not yet support non-primary keys.*`
+        );
+      }
     }
   },
 });
